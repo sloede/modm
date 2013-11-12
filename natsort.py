3c6f807d966c07d38438d6868e3ab355ecdbbc77
Michael Schlottke <michael.schlottke@sloede.com>
2013-11-12 13:27:00 +0100

    Add docstrings, comments and proper code formatting.

--- natsort.py
+++ natsort.py
@@ -25,14 +25,25 @@ import re
 
 def natsorted(l, **args):
     """Sort the given iterable in a way that humans expect."""
+    # Define method to convert items to integers
     convert = lambda s: int(s) if s.isdigit() else s
+
+    # Return None if 'cmp' is set since it is not supported
     if 'cmp' in args:
         return None
+
+    # Define lambda `keyfun` to translate values if a `key` argument
+    # was specified
     if 'key' in args:
         keyfun = args['key']
         del args['key']
     else:
         keyfun = lambda x: x
+
+    # Define lambda to get alphanumeric key
     alphanum_key = lambda key: [convert(c) for c in
             re.split('([0-9]+)', keyfun(key))]
+
+    # Return new list that is sorted naturally, while passing on all
+    # other arguments to `sorted()`
     return sorted(l, key=alphanum_key, **args)
